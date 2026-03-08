5d96818686a394e79d7a8622fffa093cbf840df5
Aditya-Ramola <workwithaditya001@gmail.com>
2026-03-08 23:33:11 +0530

    added hooks

--- apps/hooks/src/index.ts
+++ apps/hooks/src/index.ts
@@ -1,13 +1,36 @@
-import express from "express";
+import express from "express"
+import {PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-app.post("/", (req, res) => {
-    console.log(req.body);
-    res.send("Hello World!");
-});
+// https://hooks.zapier.com/hooks/catch/17043103/22b8496/
+// password logic
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
-});
+    // store in db a new trigger
+    await client.$transaction(async tx => {
+        const run = await tx.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });;
+
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+    res.json({
+        message: "Webhook received"
+    })
+})
+
+app.listen(3002);
