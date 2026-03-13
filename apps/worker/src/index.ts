b0fb56fd6051ca142f4409a08ddd3fb0a3d6e18a
Aditya-Ramola <workwithaditya001@gmail.com>
2026-03-13 23:23:07 +0530

    added worker

---
+++ apps/worker/src/index.ts
@@ -0,0 +1,18 @@
+import { Kafka } from "kafkajs";
+const TOPIC_NAME = "zap-events"
+const kafka = new Kafka({
+    clientId : 'outbox-processor',
+    brokers : ["localhost:9092"]
+})
+
+async function main() {
+    const consumer = kafka.consumer({ groupId: "main-worker" });
+    await consumer.connect();
+    await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log(message.value?.toString());
+        }
+    })
+}
